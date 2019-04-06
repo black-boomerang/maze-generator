d12b6b3e1fd4eff1622ce899e6145b42adc26fd7
black-boomerang <alejo2000@yandex.ru>
2019-04-06 20:30:06 +0300

    Исправлены недочёты, добавлен графический интерфейс

--- maze.py
+++ maze.py
@@ -1,8 +1,6 @@
 from colorama import Back, init
 from enum import Enum
 
-init()  # инициализация colorama
-
 
 class Cell_Type(Enum):  # тип клетки лабиринта
     USUAL = 0
@@ -12,23 +10,23 @@ class Cell_Type(Enum):  # тип клетки лабиринта
 
 
 class Cell:  # клетка лабиринта
-    def __init__(self, type=Cell_Type.USUAL, right_wall=True, bottom_wall=True):
+    def __init__(self, cl_type=Cell_Type.USUAL, right_wall=True, bottom_wall=True):
         self.right_wall = right_wall
         self.bottom_wall = bottom_wall
-        self.type = type
+        self.cell_type = cl_type
 
     def change_state(self, new_state=Cell_Type.USUAL):  # изменение типа клетки
-        if self.type == Cell_Type.USUAL:
-            self.type = new_state
+        if self.cell_type == Cell_Type.USUAL:
+            self.cell_type = new_state
 
 
 class Maze:
-    def __init__(self, height=2, width=2, start=(0, 0), finish=(0, 0)):
+    def __init__(self, height=2, width=2, start=None, finish=None):
         self.field = [[Cell(Cell_Type.USUAL) for j in range(width)] for i in range(height)]
-        self.start = start  # стартовая позиция
-        self.finish = (height - 1, width - 1) if finish == (0, 0) else finish  # финишная позиция
-        self.field[start[0]][start[1]].type = Cell_Type.START
-        self.field[finish[0]][finish[1]].type = Cell_Type.FINISH
+        self.start = start if start else (0, 0)  # стартовая позиция
+        self.finish = finish if finish else (height - 1, width - 1)  # финишная позиция
+        self.field[start[0]][start[1]].cell_type = Cell_Type.START
+        self.field[finish[0]][finish[1]].cell_type = Cell_Type.FINISH
         self.height = height
         self.width = width
         self.is_show_path = False  # нужно ли показывать путь
@@ -39,53 +37,54 @@ class Maze:
         else:
             return self.field[item[0]][item[1]]
 
-    def __repr__(self):  # отображение лабиринта в консоли
-        repr = ""
+    def show(self):  # отображение лабиринта в консоли
+        init()  # инициализация colorama
         for i in range(self.width * 2 + 1):  # первый слой стен
-            repr += Back.WHITE + '  ' + Back.RESET
-        repr += '\n'
+            print(Back.WHITE, '  ', Back.RESET, sep='', end='')
+        print()
 
         for row_num in range(self.height):  # вывод каждой строки лабирнта
             row = self.field[row_num]
             next_row = self.field[row_num + 1] if row_num < self.height - 1 else None
-            repr += Back.WHITE + '  ' + Back.RESET
+            print(Back.WHITE, '  ', Back.RESET, sep='', end='')
             for cell_num in range(self.width):  # вывод каждой клетки лабиринта
                 cell = row[cell_num]
                 next_cell = row[cell_num + 1] if cell_num < self.width - 1 else None
-                if cell.type == Cell_Type.START:  # вывод стартовой клетки
-                    repr += Back.GREEN
-                elif cell.type == Cell_Type.FINISH:  # вывод финишной клетки
-                    repr += Back.BLUE
-                elif self.is_show_path and cell.type == Cell_Type.IN_PATH:  # клетка. включённая в решение лабиринта
-                    repr += Back.RED
-                repr += '  ' + Back.RESET
+                if cell.cell_type == Cell_Type.START:  # вывод стартовой клетки
+                    print(Back.GREEN, end='')
+                elif cell.cell_type == Cell_Type.FINISH:  # вывод финишной клетки
+                    print(Back.BLUE, end='')
+                elif self.is_show_path and cell.cell_type == Cell_Type.IN_PATH:  # клетка. включённая в решение лабиринта
+                    print(Back.RED, end='')
+                print('  ', Back.RESET, sep='', end='')
 
                 # если две соседние клетки принадлежат решению и между ними нет стены, окрашиваем переход между ними
-                if self.is_show_path and cell.type != Cell_Type.USUAL and not cell.right_wall \
-                        and next_cell.type != Cell_Type.USUAL:
-                    repr += Back.RED
+                if self.is_show_path and cell.cell_type != Cell_Type.USUAL and not cell.right_wall \
+                        and next_cell.cell_type != Cell_Type.USUAL:
+                    print(Back.RED, end='')
                 elif cell.right_wall:  # "вертикальная" стена между клетками
-                    repr += Back.WHITE
-                repr += '  '
-                repr += Back.RESET
+                    print(Back.WHITE, end='')
+                print('  ', Back.RESET, sep='', end='')
 
-            repr += '\n' + Back.WHITE + '  ' + Back.RESET  # вывод левой боковой стены
-            for cell_num in range(self.width):  # выводим вертикальные стены между клетками, если они есть
+            print('\n', Back.WHITE, '  ', Back.RESET, sep='', end='')  # вывод левой боковой стены
+            for cell_num in range(self.width):  # выводим "горизонтальные" стены между клетками, если они есть
                 cell = row[cell_num]
                 next_cell = next_row[cell_num] if next_row is not None else None
 
                 # если две соседние клетки принадлежат решению и между ними нет стены, окрашиваем переход между ними
-                if self.is_show_path and cell.type != Cell_Type.USUAL and not cell.bottom_wall \
-                        and next_cell.type != Cell_Type.USUAL:
-                    repr += Back.RED
+                if self.is_show_path and cell.cell_type != Cell_Type.USUAL and not cell.bottom_wall \
+                        and next_cell.cell_type != Cell_Type.USUAL:
+                    print(Back.RED, end='')
                 elif cell.bottom_wall:  # "горизонтальная" стена между клетками
-                    repr += Back.WHITE
-                repr += '  ' + Back.WHITE + '  ' + Back.RESET
-            repr += '\n'
-        return repr
+                    print(Back.WHITE, end='')
+                print('  ', Back.WHITE, '  ', Back.RESET, sep='', end='')
+            print()
 
     def size(self):  # размер лабиринта
         return (self.height, self.width)
 
     def show_path(self):  # отображение решения
         self.is_show_path = True
+
+    def hide_path(self):  # скрытие решения
+        self.is_show_path = False
--- maze_generation.py
+++ maze_generation.py
@@ -1,112 +1,68 @@
-import sys, pickle
-from maze_creations import dfs_generation, prims_generation, ellers_generation, recursive_generation, maze_passage
+import pickle, argparse, os
+import maze_gui
+from maze_creations import *
 from maze import Maze, Cell
-
-
-def show_help():  # помощь по аргументам командной строки
-    print("Аргумены указываются в следующем порядке:")
-    print("1) Вариант генерации лабиринта (по умолчанию генерация обходом в глубину):")
-    print("   dfs_generation - генерация обходом в глубину,")
-    print("   prims_generation - генерация алгоритмом Прима;")
-    print("   ellers_generation - генерация алгоритмом Эллера;")
-    print("   recursive_generation - генерация методом рекурсивного деления;")
-    print("2) Размер лабиринта: высота и ширина (в случае, если лабиринт не загружается из файла);")
-    print("3) save - в случае, если лабиринт нужно загрузить, load - в случае, если лабиринт нужно загрузить;")
-    print("4) название файла (без указания расширения);")
-    print("5) show_path - в случае, если нужно отобразить решение лабиринта.")
-    print()
 
 
 def save_maze(labyrinth, file_name):  # сохранение лабиринта в файл
     "Сохранение лабиринта в файл"
-    with open(file_name, "wb") as file:
-        pickle.dump(labyrinth.size(), file)
-        pickle.dump(labyrinth.start, file)
-        pickle.dump(labyrinth.finish, file)
-        for row in labyrinth.field:
-            for cell in row:
-                pickle.dump(cell.bottom_wall, file)
-                pickle.dump(cell.right_wall, file)
+    with open(file_name, "wb") as maze_file:
+        labyrinth.hide_path()
+        pickle.dump(labyrinth, maze_file)
 
 
 def load_maze(file_name):  # загрузка лабиринта из файла
     "Загрузка лабиринта из файла"
-    with open(file_name, "rb") as file:
-        height, width = pickle.load(file)
-        start = pickle.load(file)
-        finish = pickle.load(file)
-        labyrinth = Maze(height, width, start, finish)
-        for i in range(height):
-            for j in range(width):
-                labyrinth.field[i][j].bottom_wall = pickle.load(file)
-                labyrinth.field[i][j].right_wall = pickle.load(file)
+    if os._exists(file_name):
+        print("Файл " + file_name + " отсутсствует")
+        return None
+    with open(file_name, "rb") as maze_file:
+        labyrinth = pickle.load(maze_file)
         return labyrinth
 
 
 if __name__ == "__main__":
-    is_show = True
+    parser = argparse.ArgumentParser()
     labyrinth = None
-    cur_arg = 1
 
-    if len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'help':  # помощь по аргументам командной строки
-        show_help()
-        is_show = False
-    elif len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'load':  # в случае загрузки лабиринта
-        cur_arg += 1
-        if len(sys.argv) == cur_arg:
-            print("Укажите название файла, из которого будет загружен лабиринт\n")
-            is_show = False
-        else:
-            labyrinth = load_maze(sys.argv[cur_arg] + ".maze")
-            cur_arg += 1
+    parser.add_argument("--maze_type",
+                        help="вариант генерации лабиринта (по умолчанию генерация обходом в глубину)",
+                        choices=["dfs", "prims", "ellers", "recursive"])
+    parser.add_argument("-l", "--load", help="загрузка лабиринта из файла")
+    parser.add_argument("-s", "--save", help="сохранение лабиринта в файл")
+    parser.add_argument("--height", type=int, help="высота лабиринта")
+    parser.add_argument("--width", type=int, help="ширина лабиринта")
+    parser.add_argument("--show_path", action="store_true", help="отображение решения лабиринта")
+    parser.add_argument("-g", "--gui", action="store_true", help="графический интерфейс (прохождение пользователем)")
+    args = parser.parse_args()
+
+    if args.load:  # в случае загрузки лабиринта
+        labyrinth = load_maze(os.path.join(os.getcwd(), 'mazes', args.load + '.maze'))
     else:
-        mode = 0  # вид генерации
-        if len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'dfs_generation':
-            mode = 0
-            cur_arg += 1
-        elif len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'prims_generation':
-            mode = 1
-            cur_arg += 1
-        elif len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'ellers_generation':
-            mode = 2
-            cur_arg += 1
-        elif len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'recursive_generation':
-            mode = 3
-            cur_arg += 1
+        height = width = 10
+        if args.height:  # задаём высоту лабиринта
+            height = args.height
+        if args.width:  # задаём ширину лабиринта
+            width = args.width
 
-        if len(sys.argv) == cur_arg or not sys.argv[cur_arg].isdigit():  # задаём высоту лабиринта
-            height = 10
-        else:
-            height = int(sys.argv[cur_arg])
-            cur_arg += 1
-        if len(sys.argv) == cur_arg or not sys.argv[cur_arg].isdigit():  # задаём ширину лабиринта
-            width = 10
-        else:
-            width = int(sys.argv[cur_arg])
-            cur_arg += 1
-
-        if mode == 0:  # генерация лабиринта
-            labyrinth = dfs_generation(height, width)
-        elif mode == 1:
+        if args.maze_type == "prims":  # генерация лабиринта
             labyrinth = prims_generation(height, width)
-        elif mode == 2:
+        elif args.maze_type == "ellers":
             labyrinth = ellers_generation(height, width)
-        elif mode == 3:
+        elif args.maze_type == "recursive":
             labyrinth = recursive_generation(height, width)
         else:
-            labyrinth = None
-            print("Ошибка")
+            labyrinth = dfs_generation(height, width)
 
-        if len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'save':  # в случае сохранения лабиринта
-            cur_arg += 1
-            if len(sys.argv) == cur_arg:
-                print("Укажите название файла, в который будет сохранён лабиринт\n")
-            else:
-                save_maze(labyrinth, sys.argv[cur_arg] + ".maze")
-                cur_arg += 1
+        maze_passage(labyrinth)  # решение лабиринта
 
-    if len(sys.argv) > cur_arg and sys.argv[cur_arg] == 'show_path':  # показываем решение лабиринта
-        maze_passage(labyrinth)
+    if args.save:  # в случае сохранения лабиринта
+        save_maze(labyrinth, os.path.join(os.getcwd(), 'mazes', args.save + '.maze'))
+
+    if args.show_path:  # показываем решение лабиринта
         labyrinth.show_path()
-    if is_show:  # выводим лабирнт на экран
-        print(labyrinth)
+
+    if args.gui:  # графический интерфейс (с прохождением пользователя)
+        maze_gui.gui(labyrinth)
+    else:  # отображение в консоли
+        labyrinth.show()
